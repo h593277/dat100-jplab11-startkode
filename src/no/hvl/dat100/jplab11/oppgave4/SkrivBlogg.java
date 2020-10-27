dc67f6ac15c5a8f1b6de601c2d5a6ba64555a4ed
Markus Løtveit <68389075+EmperorNOR@users.noreply.github.com>
2020-10-27 13:09:34 +0100

    Oppgave 4
    Skriver ut innholdet i Blogg samling til fil.

--- src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
+++ src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
@@ -1,6 +1,9 @@
 package no.hvl.dat100.jplab11.oppgave4;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 
 import no.hvl.dat100.jplab11.common.TODO;
@@ -9,7 +12,22 @@ import no.hvl.dat100.jplab11.oppgave3.*;
 public class SkrivBlogg {
 
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
-
-		throw new UnsupportedOperationException(TODO.method());
+		boolean skrivfil = false;
+		try
+		{
+			File obj = new File(mappe + filnavn);
+			FileWriter writer = new FileWriter(mappe + filnavn);
+			writer.write(samling.toString());
+			writer.close();
+			skrivfil = true;
+			
+		}
+		catch(IOException e)
+		{
+			System.out.println("An error occurred.");
+			 e.printStackTrace();
+		}
+		
+		return skrivfil;
 	}
 }
