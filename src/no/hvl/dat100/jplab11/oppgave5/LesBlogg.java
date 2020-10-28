ad8201bf55b1926d07ec3c57a3134ef619032901
Markus Løtveit <68389075+EmperorNOR@users.noreply.github.com>
2020-10-28 20:33:57 +0100

    Oppgave 5
    Får noe problemer med nextInt() metoden.

--- src/no/hvl/dat100/jplab11/oppgave5/LesBlogg.java
+++ src/no/hvl/dat100/jplab11/oppgave5/LesBlogg.java
@@ -1,19 +1,9 @@
 package no.hvl.dat100.jplab11.oppgave5;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Scanner;
-
-import no.hvl.dat100.jplab11.common.TODO;
-import no.hvl.dat100.jplab11.oppgave1.*;
 import no.hvl.dat100.jplab11.oppgave2.*;
 import no.hvl.dat100.jplab11.oppgave3.*;
-
-import javax.swing.JOptionPane;
 
 public class LesBlogg {
 
@@ -22,7 +12,62 @@ public class LesBlogg {
 
 	public static Blogg les(String mappe, String filnavn) {
 
-		throw new UnsupportedOperationException(TODO.method());
+		Blogg myblogg = new Blogg();
+		try
+		{
+			File obj = new File(mappe + filnavn);
+			Scanner reader = new Scanner(mappe + filnavn);
+			Tekst[] myblogtekst = new Tekst[reader.nextInt()];
+			Bilde[] myblogbilde = new Bilde[myblogtekst.length];
+			
+			
+			for(int i = 0; i < myblogtekst.length; i++)
+			{
+				if(reader.nextLine().contains(TEKST))
+				{
+					myblogtekst[i].setId(reader.nextInt());
+					myblogtekst[i].setBruker(reader.nextLine());
+					myblogtekst[i].setDato(reader.nextLine());
+					for(int j = 0; j < reader.nextInt(); j++)
+					{
+						myblogtekst[i].doLike();
+						
+					}
+					myblogtekst[i].setTekst(reader.nextLine());
+					
+					
+				}
+				else if(reader.nextLine().contains(BILDE))
+				{
+					myblogbilde[i].setId(reader.nextInt());
+					myblogbilde[i].setBruker(reader.nextLine());
+					myblogbilde[i].setDato(reader.nextLine());
+					for(int j = 0; j < reader.nextInt(); j++)
+					{
+						myblogbilde[i].doLike();
+					}
+					myblogbilde[i].setUrl(reader.nextLine());
+				}
+				if(myblogtekst[i] != null)
+				{
+					myblogg.leggTil(myblogtekst[i]);
+				}
+				else if(myblogbilde[i] != null)
+				{
+					myblogg.leggTil(myblogbilde[i]);
+				}
+				
+			}
+			reader.close();
+		}
+		catch(Exception e)
+		{
+			System.out.println("An error occurred.");
+			 e.printStackTrace();
+		}
+		
+		
+		return myblogg;
 
 	}
 }
